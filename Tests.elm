e48693975877d07b31f348650eac50619a5917dd
Yuri Martynov <ymartynov@tradingpoint.com>
2016-07-05 15:55:44 +0300

    add tests

---
+++ Tests.elm
@@ -0,0 +1,16 @@
+import ElmTest exposing (..)
+
+import String
+import Element exposing (..)
+
+all : Test
+all =
+    suite "A Test Suite"
+        [
+            test "Addition" (assertEqual (3 + 7) 10),
+            test "String.left" (assertEqual "a" (String.left 1 "abcdefg")),
+            test "This test should fail" (assert False)
+        ]
+
+main = 
+    show (stringRunner all) |> toHtml
