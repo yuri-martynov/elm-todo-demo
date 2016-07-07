899ddd38d8a87950edfb90f4b93834cbaf315f2a
Yury Martynov <yuri.martynov@gmail.com>
2016-07-07 21:28:59 +0300

    refactor

--- todo.elm
+++ todo.elm
@@ -1,6 +1,5 @@
 port module Todo exposing (..)
 
-import TodoItem
 import Search
 import TodoEntry
 import Summary
@@ -36,43 +35,28 @@ emptyModel =
 
 type Msg
     = TodoList TodoList.Msg
-    | TodoEntryMsg TodoEntry.Msg
-    | SearchMsg Search.Msg
+    | TodoEntry TodoEntry.Msg
+    | Search Search.Msg
     | Controls Controls.Msg
 
 
 update' : Msg -> Model -> Model
 update' msg model =
-    let
-        addNewTask model =
-            case model.newTask of
-                "" ->
-                    model
+    case msg of
+        Controls msg ->
+            model |> Controls.update msg
 
-                s ->
-                    { model
-                        | nextId = model.nextId + 1
-                        , tasks = (TodoList.initItem model.nextId s) :: model.tasks
-                    }
+        TodoList msg ->
+            { model | tasks = model.tasks |> TodoList.update msg }
 
-        todoEntry msg model =
-            { model | newTask = model.newTask |> TodoEntry.update msg }
-    in
-        case msg of
-            Controls msg ->
-                model |> Controls.update msg
+        Search msg ->
+            { model | search = model.search |> Search.update msg }
 
-            TodoList msg ->
-                { model | tasks = model.tasks |> TodoList.update msg }
+        TodoEntry TodoEntry.Enter ->
+            model |> add |> (todoEntry TodoEntry.Enter)
 
-            SearchMsg msg ->
-                { model | search = model.search |> Search.update msg }
-
-            TodoEntryMsg ((TodoEntry.Enter) as msg) ->
-                model |> addNewTask |> (todoEntry msg)
-
-            TodoEntryMsg msg ->
-                todoEntry msg model
+        TodoEntry msg ->
+            todoEntry msg model
 
 
 update : Msg -> Model -> ( Model, Cmd a )
@@ -83,8 +67,8 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ map TodoEntryMsg (lazy TodoEntry.view model.newTask)
-        , map SearchMsg (lazy Search.view model.search)
+        [ map TodoEntry (lazy TodoEntry.view model.newTask)
+        , map Search (lazy Search.view model.search)
         , map Controls (lazy Controls.view model)
         , map TodoList (lazy2 TodoList.view model.tasks (Controls.filter model.search model.hideDone))
         , lazy Summary.view model.tasks
@@ -106,3 +90,23 @@ main =
         , view = view
         , subscriptions = \_ -> Sub.none
         }
+
+
+
+-- helpers
+
+
+add model =
+    case model.newTask of
+        "" ->
+            model
+
+        s ->
+            { model
+                | nextId = model.nextId + 1
+                , tasks = (TodoList.initItem model.nextId s) :: model.tasks
+            }
+
+
+todoEntry msg model =
+    { model | newTask = model.newTask |> TodoEntry.update msg }
